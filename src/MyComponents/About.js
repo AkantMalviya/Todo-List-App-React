9aaa0ddbee5872cb37d8008ea786660052e47a51
Akant Malviya <akantmalviya@outlook.com>
2025-07-20 23:41:54 +0530

    final commit

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import todo from "../images/todo.jpeg";
 
 export default function About() {
 let myStyle = {
@@ -6,12 +7,13 @@ let myStyle = {
     margin: "40px auto"
 }
   return (
-    <div className="text-center mx-5" style={myStyle}>
+    <div className="mx-5" style={myStyle}>
       <h5>About Todo App</h5>
       <p>
         This is a Todo App designed and developed by Akant Malviya, Here you can add your important task as a todo item and after completion of that task you can mark as complete or delete them.
         Thank you for using my todo web application :)
       </p>
+      <img src={todo} alt="todo image" className="d-block w-50"/>
     </div>
   );
 }
